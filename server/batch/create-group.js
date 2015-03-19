498859e36e57c31cd1b00a716922bd826edb4d8d
ishitsuka.jun <ishitsuka.jun@boi.jp>
2015-03-19 10:39:10 +0000

    Fixed batch scripts

--- server/batch/create-group.js
+++ server/batch/create-group.js
@@ -4,25 +4,31 @@
 // 3. 関数を作成し, 組合せのコストを計算
 // (メモリを考えると1ずつ通り生成してコスト計算していった方がよい)
 // 4. 最小のコストの組合せを選択
-// node create_group.js
+// node create-group.js
 
 console.log('start');
 
-var batch = require('../batch.js');
+var batch = require('../batch');
 var User = require('../api/user/user.model');
 var Group = require('../api/group/group.model');
 
-const n = 100;
-const member = 5;
+const N = 100; // ランダムに組合せを作成する回数
+const MEMBER_CHANGE_VALUE = 20;
+const MEMBER_LIMIT = 8;
 
 // 参加するユーザ一覧を取得
 // Todo: 参加するユーザーのみに絞り込む必要あり
 User.find({}, null, { timeout: false }, function(err, users) {
   var minCost = 1000000;
   var bestGroup = [];
-  for (var i = 0; i < n; i++) {
+  var userCount = users.length;
+  if (userCount < MEMBER_LIMIT) {
+    process.exit();
+  }
+  var member = (userCount < MEMBER_CHANGE_VALUE) ? 4 : 5;
+  for (var i = 0; i < N; i++) {
     var cost = 0;
-    groups = createRandomGroup(users); // blocking function
+    groups = createRandomGroup(users, member); // blocking function
     groups.forEach(function(group) {
       cost += calcCostByGroup(group); // blocking function
     });
@@ -34,39 +40,38 @@ User.find({}, null, { timeout: false }, function(err, users) {
 
   // bestGroupをDBへ保存
   bestGroup.forEach(function(bg, i) {
-    var user_ids = bg.map(function(v) { return v.id; });
-    var write_blog = (i == 0) ? true : false;
+    var userIds = bg.map(function(v) { return v.id; });
+    var writeBlog = (i == 0) ? true : false;
     var date = new Date();
     newGroup = {
       date: date,
-      write_blog: write_blog,
-      user_ids: user_ids
+      write_blog: writeBlog,
+      user_ids: userIds
     };
     Group.create(newGroup, function(err, group) {
       if (err) { console.log(err); }
       console.log('group created');
-      User.update({_id: {$in: user_ids}}, {$push: {group_ids: group.id}}, {multi: true}, function(err, user){
+      User.update({_id: {$in: userIds}}, {$push: {group_ids: group.id}}, {multi: true}, function(err, user){
         if (err) { console.log(err); }
       });
     });
   });
 });
 
-// ユーザーをシャッフルした後, 最後から5人ずつグループを作成していく
-// ユーザーが5人未満になったら, すでにできているグループに1人ずつ追加していく
-// これだと20人未満の場合, グループが上手く作成できない可能性がある
-function createRandomGroup(users) {
+// ユーザーをシャッフルした後, 最後からm人ずつグループを作成していく
+// ユーザーがm人未満になったら, すでにできているグループに1人ずつ追加していく
+function createRandomGroup(users, member) {
   groups = [];
   num = users.length;
   target = 0;
-  shuffled_users = shuffle(users);
+  shuffledUsers = shuffle(users);
 
   while (num > 0) {
     if (num >= member) {
-      groups.push(shuffled_users.slice(num - member, num));
+      groups.push(shuffledUsers.slice(num - member, num));
       num -= member;
     } else {
-      groups[target].push(shuffled_users[num - 1]);
+      groups[target].push(shuffledUsers[num - 1]);
       num--;
       target++;
     }
@@ -79,7 +84,13 @@ function calcCostByGroup(group) {
   group.forEach(function(g, i) {
     for (var j = i + 1; j < group.length; j++) {
       if (g.sex == group[j].sex) { cost += 1; }
-      if (g.occupation == group[j].occupation) { cost += 2; }
+      if (g.occupation == group[j].occupation) {
+        if (g.occupation == '役員・部長・室長') {
+          cost += 10; // 役員が同じグループに入ったらコストを高く設定
+        } else {
+          cost += 2;
+        }
+      }
       if (g.department == group[j].department) { cost += 3; }
     }
   });
