89b74913bfa92cbd7db70d5b5bb75fa4ba79b660
ishitsuka.jun <ishitsuka.jun@boi.jp>
2015-04-07 07:28:02 +0000

    Fixed creating groups batch to consider user attend status

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -10,7 +10,6 @@ var UserSchema = new Schema({
   department: String,
   sex: String,
   occupation: String,
-  group_ids: [Schema.Types.ObjectId]
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- server/batch/create-group.js
+++ server/batch/create-group.js
@@ -8,51 +8,55 @@
 
 console.log('start');
 
+var moment = require('moment');
 var batch = require('../batch').batch;
 var User = require('../api/user/user.model');
 var Group = require('../api/group/group.model');
+var Attend = require('../api/attend/attend.model');
 
 const N = 100; // ランダムに組合せを作成する回数
 const MEMBER_CHANGE_VALUE = 20;
 const MEMBER_LIMIT = 8;
 
-// 参加するユーザ一覧を取得
-// Todo: 参加するユーザーのみに絞り込む必要あり
-User.find({}, null, { timeout: false }, function(err, users) {
-  var minCost = 1000000;
-  var bestGroup = [];
-  var userCount = users.length;
-  if (userCount < MEMBER_LIMIT) {
-    process.exit();
-  }
-  var member = (userCount < MEMBER_CHANGE_VALUE) ? 4 : 5;
-  for (var i = 0; i < N; i++) {
-    var cost = 0;
-    groups = createRandomGroup(users, member); // blocking function
-    groups.forEach(function(group) {
-      cost += calcCostByGroup(group); // blocking function
-    });
-    if (cost < minCost) {
-      minCost = cost;
-      bestGroup = groups;
-    }
-  }
+var jtime = moment().utc().add(9, 'hours');
+var year = jtime.year();
+var month = jtime.month() + 1;
 
-  // bestGroupをDBへ保存
-  bestGroup.forEach(function(bg, i) {
-    var userIds = bg.map(function(v) { return v.id; });
-    var writeBlog = (i == 0) ? true : false;
-    var date = new Date();
-    newGroup = {
-      date: date,
-      write_blog: writeBlog,
-      user_ids: userIds
-    };
-    Group.create(newGroup, function(err, group) {
-      if (err) { console.log(err); }
-      console.log('group created');
-      User.update({_id: {$in: userIds}}, {$push: {group_ids: group.id}}, {multi: true}, function(err, user){
+// 参加するユーザ一覧を取得
+Attend.find({active: false, year: year, month: month}, function(err, attends) {
+  notAttendIds = attends.map(function(v) { return v.user_id; });
+  User.find({_id: { $nin: notAttendIds }}, null, { timeout: false }, function(err, users) {
+    var minCost = 1000000;
+    var bestGroup = [];
+    var userCount = users.length;
+    if (userCount < MEMBER_LIMIT) {
+      process.exit();
+    }
+    var member = (userCount < MEMBER_CHANGE_VALUE) ? 4 : 5;
+    for (var i = 0; i < N; i++) {
+      var cost = 0;
+      var groups = createRandomGroup(users, member); // blocking function
+      groups.forEach(function(group) {
+        cost += calcCostByGroup(group); // blocking function
+      });
+      if (cost < minCost) {
+        minCost = cost;
+        bestGroup = groups;
+      }
+    }
+
+    // bestGroupをDBへ保存
+    bestGroup.forEach(function(bg, i) {
+      var userIds = bg.map(function(v) { return v.id; });
+      var writeBlog = (i == 0) ? true : false;
+      var newGroup = {
+        date: jtime,
+        write_blog: writeBlog,
+        user_ids: userIds
+      };
+      Group.create(newGroup, function(err, group) {
         if (err) { console.log(err); }
+        console.log('group created');
       });
     });
   });
@@ -61,10 +65,10 @@ User.find({}, null, { timeout: false }, function(err, users) {
 // ユーザーをシャッフルした後, 最後からm人ずつグループを作成していく
 // ユーザーがm人未満になったら, すでにできているグループに1人ずつ追加していく
 function createRandomGroup(users, member) {
-  groups = [];
-  num = users.length;
-  target = 0;
-  shuffledUsers = shuffle(users);
+  var groups = [];
+  var num = users.length;
+  var target = 0;
+  var shuffledUsers = shuffle(users);
 
   while (num > 0) {
     if (num >= member) {
@@ -80,7 +84,7 @@ function createRandomGroup(users, member) {
 }
 
 function calcCostByGroup(group) {
-  cost = 0;
+  var cost = 0;
   group.forEach(function(g, i) {
     for (var j = i + 1; j < group.length; j++) {
       if (g.sex == group[j].sex) { cost += 1; }
