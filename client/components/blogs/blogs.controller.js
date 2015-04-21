89a487538444d34bab26af4ca0fe485d60fe4e47
ishitsuka.jun <ishitsuka.jun@boi.jp>
2015-04-21 15:14:48 +0900

    Fixed bugs of modal and window size

--- client/components/blogs/blogs.controller.js
+++ client/components/blogs/blogs.controller.js
@@ -1,8 +1,24 @@
 'use strict';
 
 angular.module('shufflelunchApp')
-  .controller('BlogListsCtrl', function ($scope, $http, $location) {
+  .controller('BlogListsCtrl', function ($scope, $modal, $rootScope, $http, $location) {
+    var modalInstance;
+
     $http.get('/api/blogs').success(function(blogs) {
       $scope.blogs = blogs;
+    });
+
+    $scope.showBlogDetail = function(blog) {
+      $scope.targetBlog = blog;
+      modalInstance = $modal.open({
+        templateUrl: 'modal-template.html',
+        scope: $scope
+      });
+    };
+
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+      if (modalInstance) {
+        modalInstance.dismiss();
+      }
     });
   });
